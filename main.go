44c8124e56bd277746107cf1802d8c2bcdf0124e
John-S <git@suykerbuyk.org>
2020-12-28 04:27:36 -0700

    First crack at iterating over /proc/interupts

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+const pathInterrupts = "/proc/interrupts"
+
+type IrqCount uint64
+type IrqTally struct {
+	IrqNum       uint       `json:"IrqNum"`
+	CpuIrqCounts []IrqCount `json:"CpuIrqCounts"`
+	Source       string     `json:"SouceOfInterrupt"`
+}
+
+func main() {
+	var IrqTallies []IrqTally
+	var cpuCnt int = 0
+	buff, err := ioutil.ReadFile(pathInterrupts)
+	if err != nil {
+		log.Println("could not read" + pathInterrupts)
+		return
+	}
+	lines := strings.Split(string(buff), "\n")
+	for idx, line := range lines {
+		if idx == 0 {
+			cpuCnt = len(strings.Fields(line))
+		} else {
+			if cpuCnt < 1 {
+				log.Println("Could not detect the number of CPUs")
+				os.Exit(1)
+			}
+			var irqTally IrqTally
+			splits := strings.Fields(line)
+			if _, err := fmt.Sscanf(splits[0], "%d:", &irqTally.IrqNum); err == nil {
+				for i := 1; i < (cpuCnt - 1); i++ {
+					var irqCnt IrqCount
+					fmt.Sscanf(splits[i], "%d", &irqCnt)
+					irqTally.CpuIrqCounts = append(irqTally.CpuIrqCounts, irqCnt)
+				}
+				for i := cpuCnt; i < len(splits); i++ {
+					if len(irqTally.Source) != 0 {
+						irqTally.Source += " "
+					}
+					irqTally.Source += strings.TrimSpace(splits[i])
+				}
+				IrqTallies = append(IrqTallies, irqTally)
+			} else {
+				for _, irqTally := range IrqTallies {
+					var stringOfCounts string
+					for _, irqCnt := range irqTally.CpuIrqCounts {
+						stringOfCounts += fmt.Sprintf("%d", irqCnt) + " "
+					}
+				}
+				break
+			}
+		}
+	}
+	out, err := json.Marshal(IrqTallies)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(out))
+}
